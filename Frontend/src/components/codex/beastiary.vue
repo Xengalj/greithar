5b274106c1e1d24d86d56a6bec5db437c09ffec9
Xengalj <trevor@musson.net>
2024-06-06 18:12:17 -0400

    update table

--- Frontend/src/components/codex/beastiary.vue
+++ Frontend/src/components/codex/beastiary.vue
@@ -6,22 +6,73 @@
 
     <!-- https://www.svgrepo.com/collection/game-skills-vectors/ -->
     <!-- https://element-plus.org/en-US/component/table.html -->
-    <el-button @click="clearFilter">reset all filters</el-button>
+
+    <el-row class="row-bg" justify="space-between">
+      <el-col :span="6">
+        <el-input v-model="tableSearch" size="small" placeholder="Type to search" />
+      </el-col>
+      <el-col :span="6">
+        <!--
+        <el-select v-model="value" multiple placeholder="Select" style="width: 240px">
+          <el-option
+            v-for="item in colors"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          >
+            <div class="flex items-center">
+              <el-tag :color="item.value" style="margin-right: 8px" size="small" />
+              <span :style="{ color: item.value }">{{ item.label }}</span>
+            </div>
+          </el-option>
+          <template #tag>
+            <el-tag v-for="color in value" :key="color" :color="color" />
+          </template>
+        </el-select>
+       -->
+      </el-col>
+      <el-col :span="6">
+        <el-button @click="filterHandler">Filter</el-button>
+        <el-button @click="clearFilter">Reset</el-button>
+      </el-col>
+    </el-row>
+
     <el-table
       :data="tableData"
       :default-sort="{ prop: 'cr', order: 'ascending' }"
       v-loading="loading"
+      table-layout="auto"
       height="500"
       style="width: 100%"
       stripe
     >
+
+      <el-table-column type="expand">
+        <template #default="scope">
+          <div>{{ scope.row }}</div>
+        </template>
+      </el-table-column>
+
       <el-table-column sortable prop="Name" label="Name" width="180" />
-      <el-table-column sortable prop="Type" label="Type" width="75" :filters="typeFilter" :filter-method="filterHandler" />
+
+      <el-table-column sortable prop="Type" label="Type">
+        <template #default="scope">
+          <el-row class="row-bg" justify="space-between">
+            <el-col :span="6">
+              <g-icon iconSize="32px" :iconName="scope.row.Type"/>
+            </el-col>
+            <el-col :span="17">
+              {{ scope.row.Type }}
+            </el-col>
+          </el-row>
+         </template>
+       </el-table-column>
+
       <el-table-column sortable prop="CR" label="CR" />
       <el-table-column sortable prop="HP" label="HP" />
       <el-table-column sortable prop="AC" label="AC" />
-      <el-table-column sortable prop="Alignment" label="Alignment" />
-      <el-table-column sortable prop="Environment" label="Environment" :filters="environFilter" :filter-method="filterHandler" />
+      <el-table-column sortable prop="Alignment" label="Align" />
+      <el-table-column sortable prop="Environment" label="Environment" />
     </el-table>
 
     <br><br>
@@ -32,8 +83,8 @@
     <g-icon iconSize="64px" iconName="dragon"/> Dragons<br>
     <g-icon iconSize="64px" iconName="fey"/> Fey<br>
     <g-icon iconSize="64px" iconName="humanoid"/> Humanoids<br>
-    <g-icon iconSize="64px" iconName="magicalBeast"/> Magical Beasts<br>
-    <g-icon iconSize="64px" iconName="monsterousHumanoid"/> Monsterous Humanoids<br>
+    <g-icon iconSize="64px" iconName="magical beast"/> Magical Beasts<br>
+    <g-icon iconSize="64px" iconName="monstrous humanoid"/> Monsterous Humanoids<br>
     <g-icon iconSize="64px" iconName="ooze"/> Oozes<br>
     <g-icon iconSize="64px" iconName="outsider"/> Outsiders<br>
     <g-icon iconSize="64px" iconName="plant"/> Plants<br>
@@ -55,11 +106,22 @@ export default {
     return {
       content: "Super Temp",
       loading: false,
+      tableSearch: "",
       tableData: [],
       typeFilter: [
-        { text: 'Aberrations', value: 'aberration' },
-        { text: 'Animal', value: 'animal' },
-        { text: 'Construct', value: 'construct' },
+        // { text: 'Aberrations', value: 'aberration' },
+        // { text: 'Animals', value: 'animal' },
+        // { text: 'Constructs', value: 'construct' },
+        { text: 'Dragons', value: 'dragon' },
+        // { text: 'Fey', value: 'fey' },
+        // { text: 'Humanoids', value: 'humanoid' },
+        // { text: 'Magical Beasts', value: 'magical beast' },
+        // { text: 'Monsterous Humanoids', value: 'monstrous humanoid' },
+        // { text: 'Oozes', value: 'ooze' },
+        // { text: 'Outsiders', value: 'outsider' },
+        // { text: 'Plants', value: 'plant' },
+        // { text: 'Undead', value: 'undead' },
+        // { text: 'Vermin', value: 'vermin' },
       ],
       environFilter: [
         { text: 'Any', value: 'any' },
@@ -86,6 +148,16 @@ export default {
     filterHandler( value, row, column ) {
       const property = column['property'];
       return row[property] === value;
+/*
+const filterTableData = computed(() =>
+  tableData.filter(
+    (data) =>
+      !tableSearch.value ||
+      data.name.toLowerCase().includes(tableSearch.value.toLowerCase())
+  )
+)
+*/
+
     }
   },
 };
--- Frontend/src/components/template/BaseIcon.vue
+++ Frontend/src/components/template/BaseIcon.vue
@@ -40,7 +40,7 @@ export default {
     }
   },
   mounted() {
-    this.icon = icons[this.iconName];
+    this.icon = icons[this.iconName.toLowerCase()];
   }
 }
 </script>
