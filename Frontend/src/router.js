8e46432fe7dbb00dd003f49fca1f6b73db0380e9
Xengalj <trevor@musson.net>
2025-01-16 16:31:47 -0500

    make prod actually run correctly (using hash history in vue router)

--- Frontend/src/router.js
+++ Frontend/src/router.js
@@ -1,4 +1,4 @@
-import { createWebHistory, createRouter } from "vue-router";
+import { createWebHashHistory, createRouter } from "vue-router";
 
 import Home from "./components/Home.vue";
 import About from "./components/About.vue";
@@ -275,7 +275,8 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  // history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
